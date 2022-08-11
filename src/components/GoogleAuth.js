f42bfd310d0b8be7fb7413159aaceffa01ed436f
tcocco20 <theococco@yahoo.com>
2022-08-10 19:13:19 -0700

    rendered streams to streamList

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -41,14 +41,14 @@ class GoogleAuth extends React.Component {
       return null;
     } else if (this.props.isSignedIn) {
       return (
-        <button onClick={this.onSignOutClick} className="ui blue google button">
+        <button onClick={this.onSignOutClick} className="ui green google button">
           <i className="google icon" />
           Sign Out
         </button>
       );
     } else {
       return (
-        <button onClick={this.onSignInClick} className="ui blue google button">
+        <button onClick={this.onSignInClick} className="ui green google button">
           <i className="google icon" />
           Sign In
         </button>
