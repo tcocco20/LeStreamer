961d7d66a6daf77b99bb0ce0ca4a5bd3cf3f9a23
tcocco20 <theococco@yahoo.com>
2022-08-12 11:49:07 -0700

    refactored modal, started adding delete handling

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,11 +1,24 @@
 import React from "react";
+import history from "../../history";
 import Modal from "../Modal";
 
 const StreamDelete = () => {
+  const actions = (
+    <>
+      <button className="ui button negative">Delete</button>
+      <button className="ui button">Cancel</button>
+    </>
+  );
+
   return (
     <div>
       StreamDelete
-      <Modal />
+      <Modal
+        title="Delete Stream"
+        content="Are you sure you want to delete this stream?"
+        actions={actions}
+        onDismiss={() => history.push("/")}
+      />
     </div>
   );
 };
